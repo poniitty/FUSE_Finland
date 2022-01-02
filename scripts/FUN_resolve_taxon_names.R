74a98d647f0e732bd6e3888a279e7f2f84b0e126
poniitty <pekka.niittynen@helsinki.fi>
2022-01-02 13:23:23 +0100

    Hybrid handling added

--- scripts/FUN_resolve_taxon_names.R
+++ scripts/FUN_resolve_taxon_names.R
@@ -1,5 +1,5 @@
 # orig_name <- "Betula nana"
-orig_name <- "Dactylorhiza majalis subsp. sphagncola"
+# orig_name <- "Senecio sylvatcus × vulgaris"
 # dataset_key <- "bae5856f-da10-4333-90a0-5a2135361b30"
 # dataset <- c("f382f0ce-323a-4091-bb9f-add557f3a9a2","d9a4eedb-e985-4456-ad46-3df8472e00e8")
 # resolve_taxon_name(orig_name)
@@ -291,46 +291,29 @@ resolve_taxon_name <- function(orig_name, dataset = NULL, lib.loc = .libPaths(),
               } else {
                 i <- gbif_parse(i)$canonicalnamecomplete
                 nl <- name_suggest(q=i, datasetKey = dataset_key)$data
-                if(nrow(nl) > 0){
+                if(nrow(nl) > 0 & nrow(nl) != 100){
                   stop()
                 } else {
-                  i <- gbif_parse(i)$canonicalname
-                  nl <- name_suggest(q=i, datasetKey = dataset_key)$data
-                  if(nrow(nl) > 0){
-                    stop()
+                  if(nrow(nl) == 100 & grepl("×", orig_name)){
+                    i <- orig_name
+                    NAME <- NA
+                    STATUS <- NA
+                    NOTE = "HYBRID TAXA! RESOLVING FAILED"
+                    SPEC_NAME <- NA
                   } else {
                     i <- gbif_parse(i)$canonicalname
-                    if(length(strsplit(i, " ")[[1]]) > 2){
-                      i <- paste(strsplit(i, " ")[[1]][1:2], collapse = " ")
-                      nl <- name_suggest(q=i, datasetKey = dataset_key)$data
-                      if(nrow(nl) > 0){
-                        if(nrow(nl) == 1){
-                          nl <- name_usage(key=nl$key)$data
-                          if(!nl$synonym){
-                            if(nl$taxonomicStatus == "ACCEPTED"){
-                              NAME <- nl$scientificName
-                              STATUS <- nl$taxonomicStatus
-                              NOTE = "PARSED TO SPECIES"
-                              if(nl$rank == "SPECIES"){
-                                SPEC_NAME <- nl$scientificName
-                              } else {
-                                if(nl$rank %in% c("FORM","SUBSPECIES","VARIETY")){
-                                  if("speciesKey" %in% names(nl)){
-                                    nl <- name_usage(key=nl$speciesKey)$data
-                                    SPEC_NAME <- nl$scientificName
-                                  } else {
-                                    stop()
-                                  }
-                                } else {
-                                  stop()
-                                }
-                              }
-                            } else {
-                              stop()
-                            }
-                          } else {
-                            if("acceptedKey" %in% names(nl)){
-                              nl <- name_usage(key=nl$acceptedKey)$data
+                    nl <- name_suggest(q=i, datasetKey = dataset_key)$data
+                    if(nrow(nl) > 0){
+                      stop()
+                    } else {
+                      i <- gbif_parse(i)$canonicalname
+                      if(length(strsplit(i, " ")[[1]]) > 2){
+                        i <- paste(strsplit(i, " ")[[1]][1:2], collapse = " ")
+                        nl <- name_suggest(q=i, datasetKey = dataset_key)$data
+                        if(nrow(nl) > 0){
+                          if(nrow(nl) == 1){
+                            nl <- name_usage(key=nl$key)$data
+                            if(!nl$synonym){
                               if(nl$taxonomicStatus == "ACCEPTED"){
                                 NAME <- nl$scientificName
                                 STATUS <- nl$taxonomicStatus
@@ -353,42 +336,42 @@ resolve_taxon_name <- function(orig_name, dataset = NULL, lib.loc = .libPaths(),
                                 stop()
                               }
                             } else {
-                              stop()
-                            }
-                          }
-                        } else {
-                          if(gbif_parse(i)$canonicalname %in% nl$canonicalName){
-                            nl <- nl[nl$canonicalName == gbif_parse(i)$canonicalname & (!is.na(nl$canonicalName)),]
-                            if(nrow(nl) == 1){
-                              nl <- name_usage(key=nl$key)$data
-                              if(nrow(nl) > 1){nl <- nl[!duplicated(nl),]}
-                              
-                              if(nrow(nl) == 1){
-                                if(!nl$synonym){
-                                  if(nl$taxonomicStatus == "ACCEPTED"){
-                                    NAME <- nl$scientificName
-                                    STATUS <- nl$taxonomicStatus
-                                    NOTE = "PARSED TO SPECIES"
-                                    if(nl$rank == "SPECIES"){
-                                      SPEC_NAME <- nl$scientificName
-                                    } else {
-                                      if(nl$rank %in% c("FORM","SUBSPECIES","VARIETY")){
-                                        if("speciesKey" %in% names(nl)){
-                                          nl <- name_usage(key=nl$speciesKey)$data
-                                          SPEC_NAME <- nl$scientificName
-                                        } else {
-                                          stop()
-                                        }
+                              if("acceptedKey" %in% names(nl)){
+                                nl <- name_usage(key=nl$acceptedKey)$data
+                                if(nl$taxonomicStatus == "ACCEPTED"){
+                                  NAME <- nl$scientificName
+                                  STATUS <- nl$taxonomicStatus
+                                  NOTE = "PARSED TO SPECIES"
+                                  if(nl$rank == "SPECIES"){
+                                    SPEC_NAME <- nl$scientificName
+                                  } else {
+                                    if(nl$rank %in% c("FORM","SUBSPECIES","VARIETY")){
+                                      if("speciesKey" %in% names(nl)){
+                                        nl <- name_usage(key=nl$speciesKey)$data
+                                        SPEC_NAME <- nl$scientificName
                                       } else {
                                         stop()
                                       }
+                                    } else {
+                                      stop()
                                     }
-                                  } else {
-                                    stop()
                                   }
                                 } else {
-                                  if("acceptedKey" %in% names(nl)){
-                                    nl <- name_usage(key=nl$acceptedKey)$data
+                                  stop()
+                                }
+                              } else {
+                                stop()
+                              }
+                            }
+                          } else {
+                            if(gbif_parse(i)$canonicalname %in% nl$canonicalName){
+                              nl <- nl[nl$canonicalName == gbif_parse(i)$canonicalname & (!is.na(nl$canonicalName)),]
+                              if(nrow(nl) == 1){
+                                nl <- name_usage(key=nl$key)$data
+                                if(nrow(nl) > 1){nl <- nl[!duplicated(nl),]}
+                                
+                                if(nrow(nl) == 1){
+                                  if(!nl$synonym){
                                     if(nl$taxonomicStatus == "ACCEPTED"){
                                       NAME <- nl$scientificName
                                       STATUS <- nl$taxonomicStatus
@@ -411,57 +394,19 @@ resolve_taxon_name <- function(orig_name, dataset = NULL, lib.loc = .libPaths(),
                                       stop()
                                     }
                                   } else {
-                                    stop()
-                                  }
-                                }
-                              } else {
-                                stop()
-                              }
-                            } else {
-                              if(nrow(nl) > 1){
-                                success <- FALSE
-                                for(ikey in nl$key){
-                                  nl1 <- name_usage(key=ikey)$data
-                                  if(nl1$taxonomicStatus == "ACCEPTED"){
-                                    success <- TRUE
-                                    NAME <- nl1$scientificName
-                                    STATUS <- nl1$taxonomicStatus
-                                    NOTE = "PARSED TO SPECIES"
-                                    if(nl1$rank == "SPECIES"){
-                                      SPEC_NAME <- nl1$scientificName
-                                    } else {
-                                      if(nl1$rank %in% c("FORM","SUBSPECIES","VARIETY")){
-                                        if("speciesKey" %in% names(nl1)){
-                                          nl1 <- name_usage(key=nl1$speciesKey)$data
-                                          SPEC_NAME <- nl1$scientificName
-                                        } else {
-                                          stop()
-                                        }
-                                      } else {
-                                        stop()
-                                      }
-                                    }
-                                    break
-                                  }
-                                }
-                                if(success == FALSE){
-                                  for(ikey in nl$key){
-                                    nl1 <- name_usage(key=ikey)$data
-                                    if("acceptedKey" %in% names(nl1)){
-                                      nl1 <- name_usage(key=nl1$acceptedKey)$data
-                                      
-                                      if(nl1$taxonomicStatus == "ACCEPTED"){
-                                        success <- TRUE
-                                        NAME <- nl1$scientificName
-                                        STATUS <- nl1$taxonomicStatus
+                                    if("acceptedKey" %in% names(nl)){
+                                      nl <- name_usage(key=nl$acceptedKey)$data
+                                      if(nl$taxonomicStatus == "ACCEPTED"){
+                                        NAME <- nl$scientificName
+                                        STATUS <- nl$taxonomicStatus
                                         NOTE = "PARSED TO SPECIES"
-                                        if(nl1$rank == "SPECIES"){
-                                          SPEC_NAME <- nl1$scientificName
+                                        if(nl$rank == "SPECIES"){
+                                          SPEC_NAME <- nl$scientificName
                                         } else {
-                                          if(nl1$rank %in% c("FORM","SUBSPECIES","VARIETY")){
-                                            if("speciesKey" %in% names(nl1)){
-                                              nl1 <- name_usage(key=nl1$speciesKey)$data
-                                              SPEC_NAME <- nl1$scientificName
+                                          if(nl$rank %in% c("FORM","SUBSPECIES","VARIETY")){
+                                            if("speciesKey" %in% names(nl)){
+                                              nl <- name_usage(key=nl$speciesKey)$data
+                                              SPEC_NAME <- nl$scientificName
                                             } else {
                                               stop()
                                             }
@@ -469,27 +414,90 @@ resolve_taxon_name <- function(orig_name, dataset = NULL, lib.loc = .libPaths(),
                                             stop()
                                           }
                                         }
-                                        break
+                                      } else {
+                                        stop()
+                                      }
+                                    } else {
+                                      stop()
+                                    }
+                                  }
+                                } else {
+                                  stop()
+                                }
+                              } else {
+                                if(nrow(nl) > 1){
+                                  success <- FALSE
+                                  for(ikey in nl$key){
+                                    nl1 <- name_usage(key=ikey)$data
+                                    if(nl1$taxonomicStatus == "ACCEPTED"){
+                                      success <- TRUE
+                                      NAME <- nl1$scientificName
+                                      STATUS <- nl1$taxonomicStatus
+                                      NOTE = "PARSED TO SPECIES"
+                                      if(nl1$rank == "SPECIES"){
+                                        SPEC_NAME <- nl1$scientificName
+                                      } else {
+                                        if(nl1$rank %in% c("FORM","SUBSPECIES","VARIETY")){
+                                          if("speciesKey" %in% names(nl1)){
+                                            nl1 <- name_usage(key=nl1$speciesKey)$data
+                                            SPEC_NAME <- nl1$scientificName
+                                          } else {
+                                            stop()
+                                          }
+                                        } else {
+                                          stop()
+                                        }
+                                      }
+                                      break
+                                    }
+                                  }
+                                  if(success == FALSE){
+                                    for(ikey in nl$key){
+                                      nl1 <- name_usage(key=ikey)$data
+                                      if("acceptedKey" %in% names(nl1)){
+                                        nl1 <- name_usage(key=nl1$acceptedKey)$data
+                                        
+                                        if(nl1$taxonomicStatus == "ACCEPTED"){
+                                          success <- TRUE
+                                          NAME <- nl1$scientificName
+                                          STATUS <- nl1$taxonomicStatus
+                                          NOTE = "PARSED TO SPECIES"
+                                          if(nl1$rank == "SPECIES"){
+                                            SPEC_NAME <- nl1$scientificName
+                                          } else {
+                                            if(nl1$rank %in% c("FORM","SUBSPECIES","VARIETY")){
+                                              if("speciesKey" %in% names(nl1)){
+                                                nl1 <- name_usage(key=nl1$speciesKey)$data
+                                                SPEC_NAME <- nl1$scientificName
+                                              } else {
+                                                stop()
+                                              }
+                                            } else {
+                                              stop()
+                                            }
+                                          }
+                                          break
+                                        }
                                       }
                                     }
                                   }
+                                  if(success == FALSE){
+                                    NAME <- NA
+                                    STATUS <- NA
+                                    NOTE = "NOT FOUND"
+                                    SPEC_NAME <- NA
+                                  }
+                                } else {
+                                  stop()
                                 }
-                                if(success == FALSE){
-                                  NAME <- NA
-                                  STATUS <- NA
-                                  NOTE = "NOT FOUND"
-                                  SPEC_NAME <- NA
-                                }
-                              } else {
-                                stop()
                               }
+                            } else {
+                              stop()
                             }
-                          } else {
-                            stop()
                           }
+                        } else {
+                          stop()
                         }
-                      } else {
-                        stop()
                       }
                     }
                   }
@@ -834,10 +842,14 @@ resolve_taxon_name <- function(orig_name, dataset = NULL, lib.loc = .libPaths(),
                 }
                 
               } else {
-                NAME <- NA
-                STATUS <- NA
-                NOTE = "NOT FOUND"
-                SPEC_NAME <- NA
+                if(is.null(NAME)){
+                  NAME <- NA
+                  STATUS <- NA
+                  NOTE = "NOT FOUND"
+                  SPEC_NAME <- NA
+                } else {
+                  stop()
+                }
               }
             }
           },silent=TRUE)
@@ -861,7 +873,15 @@ resolve_taxon_name <- function(orig_name, dataset = NULL, lib.loc = .libPaths(),
           spec_parsed <- NA
         } else {
           parsed <- parsenames(NAME)$canonicalnamewithmarker
-          spec_parsed <- paste(strsplit(parsenames(NAME)$canonicalname, " ")[[1]][1:2], collapse = " ")
+          spec_parsed <- try(paste(strsplit(parsenames(NAME)$canonicalname, " ")[[1]][1:2], collapse = " "), silent = T)
+          if(is.null(parsed) & parsenames(NAME)$type == "HYBRID"){
+            parsed <- NA
+            spec_parsed <- NA
+            NOTE <- "HYBRID! RESOLVING MIGHT BE UNRELIABLE"
+          }
+          if(class(spec_parsed) == "try-error"){
+            spec_parsed <- NA
+          }
         }
         
         resolved_temp2 <- data.frame(orig_name = orig_name,
